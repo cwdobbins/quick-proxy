17054450417f14276797d29cff93764a28dc1279
Christopher Dobbins <christopher.dobbins@culebrasoftware.com>
2018-11-01 21:18:51 -0400

    initial commit

---
+++ quick-proxy.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var proxy = require('http-proxy-middleware');
+var app = express();
+var argv = require('minimist')(process.argv.slice(2));
+
+
+var rewritePath = `^${argv.path}`;
+
+var config = {
+    target: argv.target,
+    secure: argv.secure || true,
+    pathRewrite: { [rewritePath]: '' },
+    changeOrigin: true,
+    logLevel: "debug"
+};
+
+app.use(argv.path, proxy(config));
+app.listen(argv.localPort);
