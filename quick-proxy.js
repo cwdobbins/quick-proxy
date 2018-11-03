0e1d869a076c107a484fd731973c42ef1b265881
Christopher Dobbins <christopher.dobbins@culebrasoftware.com>
2018-11-03 10:26:00 +0000

    Added headers to CORS-enable

--- quick-proxy.js
+++ quick-proxy.js
@@ -3,16 +3,19 @@ var proxy = require('http-proxy-middleware');
 var app = express();
 var argv = require('minimist')(process.argv.slice(2));
 
-
-var rewritePath = `^${argv.path}`;
-
 var config = {
     target: argv.target,
     secure: argv.secure || true,
-    // pathRewrite: { [rewritePath]: '' },
     changeOrigin: true,
-    logLevel: "debug"
+    logLevel: "info"
 };
+
+app.use(function(req, res, next) {
+	  res.header("Access-Control-Allow-Origin", req.header('Origin'));
+	  res.header("Access-Control-Allow-Methods", "*");
+	  res.header("Access-Control-Allow-Headers", "*");
+	  next();
+});
 
 app.use('/', proxy(config));
 app.listen(argv.localPort);
